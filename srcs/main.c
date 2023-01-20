c302c48f267f1fd763c7e4935a95ec5e686f231a
adbenoit-9 <adeline.benoit@hotmail.com>
2023-01-20 17:00:14 +0100

    [CLEAN]

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: adbenoit <adbenoit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 14:32:23 by adbenoit          #+#    #+#             */
-/*   Updated: 2023/01/20 16:51:36 by adbenoit         ###   ########.fr       */
+/*   Updated: 2023/01/20 17:00:03 by adbenoit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,6 @@ void    *get_shared_mem()
         }
         else if (isset == 0) {
             memset((char *)ptr, '0', MAP_LENGTH * MAP_WIDTH);
-            ((char *)ptr)[0] = '1';
         }
     }
     return (ptr);    
